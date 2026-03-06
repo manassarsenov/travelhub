35cde24ec58045b16e389366a9a9ccc462406f7c
ManasSarsenov <manassarsenov186@gmail.com>
2026-03-06 17:08:05 +0500

    change

--- apps/models/__init__.py
+++ apps/models/__init__.py
@@ -1,5 +1,6 @@
 from apps.models.activities import Activity
 from apps.models.countries import Country
 from apps.models.destinations import Destination
+from apps.models.reviews import Review
 from apps.models.tags import Tag
 from apps.models.users import User
---
+++ apps/models/reviews.py
@@ -0,0 +1,32 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db.models import TextChoices, ForeignKey, CASCADE, SET_NULL, CharField, PositiveIntegerField, \
+    PositiveSmallIntegerField, TextField, DateTimeField, BooleanField
+
+from apps.models.base import CreatedBaseModel
+
+
+class Review(CreatedBaseModel):
+    class VisitType(TextChoices):
+        FAMILY = 'family', 'Visited with family'
+        PARTNER = 'partner', 'Visited with a partner'
+        SOLO = 'solo', 'Solo traveller'
+        FRIENDS = 'friends', 'Visited with friends'
+
+    destination = ForeignKey('apps.Destination', CASCADE, related_name='reviews')
+    user = ForeignKey('apps.User', SET_NULL, null=True, blank=True, related_name='reviews')
+
+    author_name = CharField(max_length=100, blank=True)
+    author_country = ForeignKey('apps.Country', SET_NULL, null=True, blank=True, related_name='reviews')
+
+    rating = PositiveSmallIntegerField(default=5, validators=[MinValueValidator(1), MaxValueValidator(5)],
+                                       help_text='1 dan 5 gacha')
+
+    text = TextField()
+    visit_type = CharField(max_length=20, choices=VisitType.choices, blank=True)
+    visited_at = DateTimeField(null=True, blank=True)
+    is_visible = BooleanField(default=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name = 'Review'
+        verbose_name_plural = 'Reviews'
